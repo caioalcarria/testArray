ac0101c629e7557f7e821bfeb618d2e340bde098
Caio Alcarria <caio.alcarria@systema.com>
2025-01-23 09:31:47 -0300

    v5

--- js/components/header.js
+++ js/components/header.js
@@ -2,7 +2,7 @@ export default function  header (){
     let header = document.createElement('header')
   
     let logo = document.createElement('img')
-    logo.src="/assets/SYSTEMA_Logo_Claim_RGB.svg"
+    logo.src="assets/SYSTEMA_Logo_Claim_RGB.svg"
     header.appendChild(logo)
   
     return header
